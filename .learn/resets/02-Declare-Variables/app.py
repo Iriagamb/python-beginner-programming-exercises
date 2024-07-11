d2fac9e6f36179435914eac7f7be4d7cb58dead2
Gabriela Miralles <iria30fuceva@gmail.com>
2024-07-11 15:36:47 +0000

    exercises

--- .learn/resets/02-Declare-Variables/app.py
+++ .learn/resets/02-Declare-Variables/app.py
@@ -1,3 +1,1 @@
 # ✅ ↓ your code here ↓ ✅
-color="yellor"
-print(color)
--- .learn/resets/07-Create-a-Basic-HTML/app.py
+++ .learn/resets/07-Create-a-Basic-HTML/app.py
@@ -10,5 +10,4 @@ h = '<body>'
 # ❌ ⬆ DON'T CHANGE THE CODE ABOVE ⬆ ❌
 
 # ✅ ↓ start coding below here ↓ ✅
-html_document = a + c + g + f + h + d + b
-print(html_document)
+
